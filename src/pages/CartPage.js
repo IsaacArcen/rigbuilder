a0fd0124e60694e933e92cbcd9e06609722525e9
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-05-21 15:22:32 +0200

    cartpage - design

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -5,4 +5,21 @@ const componentLabels = {
     gpu: "GPU",
     cpu: "CPU",
     motherboard: "Motherboard",
+    ram: "RAM",
+    storage: "Storage",
+    coolong: "Cooling",
+    case: "Case",
+    extra: "Extra",
+};
+
+// Cartpage visar användarens komponenter innan checkout
+
+function CartPage() {
+    const {build, removeComponent, clearbuild} = useBuild();
+
+    const selectedItems = Object.entries(build).filter(([, product]) => product);
+
+    const totalPrice = selectedItems.reduce((sum, [, product]) => {
+        return sum + product.price;
+    })
 }
