93d4ecc59c25995668662ebd7cc22c5ced5beb5d
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-05-24 19:58:44 +0200

    checkout

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,1 +1,46 @@
-import 
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useBuild } from "../context/BuildContext";
+
+const componentLabels = {
+    gpu: "GPU",
+    cpu: "CPU",
+    motherboard: "Motherboard",
+    ram: "RAM",
+    storage: "Storage",
+    coolong: "Cooling",
+    case: "Case",
+    extra: "Extra",
+};
+
+//Checkoutsidan med formulär för kunduppgifter
+function CheckoutPage() {
+
+    const { build } = useBuild();
+
+    const selectedItems = Object.entries(build).filter(([, product]) => product);
+
+    const totalPrice = selectedItems.reduce((sum, [, product]) => {
+        return sum + product.price;
+    }, 0);
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        paymentmethod: "",
+    });
+
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const inputChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormData((currentData) => ({
+            ...currentData,
+            [name]: value,
+        }));
+    };
+
+    
+}
