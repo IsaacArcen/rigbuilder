39339d937e9946ac26e5e1510e68e8a8f20fd29f
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-05-25 17:06:52 +0200

    const writeOrders

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -9,3 +9,9 @@ const readOrders = () => {
     const orderData = fs.readFileSync(ordersFilePath, "utf8");
     return JSON.parse(orderData);
 };
+
+//Skriver uppdaterade orders till JSON.filen
+const writeOrders = (orders) => {
+    fs.writeFileSync(ordersFilePath, JSON.stringify(orders, null, 2));
+};
+
