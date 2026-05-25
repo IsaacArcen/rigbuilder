22baf38cb789a00822d30d20bcfd149ca15142fe
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-05-25 17:28:17 +0200

    GET /api/orders = kör getOrders POST /api/orders = createOrder

--- server/routers/orderRoutes.js
+++ server/routers/orderRoutes.js
@@ -7,3 +7,10 @@ const {
     getOrders,
 } = require("../controllers/orderController");
 
+//Route för api/orders
+//GET hämtar alla orders.
+//POST skapar ny order.
+router.route("/").get(getOrders).post(createOrder);
+
+//export
+module.exports = router;
