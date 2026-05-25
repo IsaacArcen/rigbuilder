d8f66531f88c0baf429482afc1d3fdba8cbf8ed8
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-05-25 17:05:39 +0200

    const readOrders

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -3,3 +3,9 @@ const fs = require("fs");
 const path = require("path");
 
 const ordersFilePath = path.join(__dirname, "../data/orders.json");
+
+//Läser alla orders i JSON-filen
+const readOrders = () => {
+    const orderData = fs.readFileSync(ordersFilePath, "utf8");
+    return JSON.parse(orderData);
+};
