3556bcbd1ed25f66efd673cae5ae9ce240ddfc91
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-05-28 16:42:38 +0200

    func getToken

--- src/api.js
+++ src/api.js
@@ -1,3 +1,9 @@
 //Bas url till backend
 // react kör localhost 3000 backend kör 5000
 const API_BASE = "http://localhost:5000/api";
+
+//hämtar jwt token från localstorage
+//sparas när user loggar in
+function getToken() {
+    return localStorage.getItem("token");
+}
