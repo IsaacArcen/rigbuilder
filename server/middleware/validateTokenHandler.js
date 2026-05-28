de3c2a656bb25928fd3128c5fa26629af783f2c5
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-05-28 15:44:39 +0200

    const validateToken

--- server/middleware/validateTokenHandler.js
+++ server/middleware/validateTokenHandler.js
@@ -1,2 +1,27 @@
 const asyncHandler = require("express-async-handler");
 const jwt = requre("jsonwebtoken");
+
+//middleware som kontrollerar om request har en giltig jwt-token
+const validateToken = asyncHandler(async (req, res, next) => {
+    let token;
+
+    //läser auth-headern från requesten
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+
+    if (authHeader && authHeader.startsWith("Bearer")) {
+        token = authHeader.split(" ")[1];
+
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                res.status(401);
+                throw new Error("User is not authorized");
+            }
+
+            req.user = decoded.user;
+            next();
+        });
+    } else {
+        res.status(401);
+        throw new Error("Token is missing or malformed");
+    }
+});
