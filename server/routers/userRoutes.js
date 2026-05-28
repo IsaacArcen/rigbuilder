530f384cdc1ea4c3967e1524f273b5de3f915fc2
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-05-28 16:05:05 +0200

    routers + module.exports

--- server/routers/userRoutes.js
+++ server/routers/userRoutes.js
@@ -11,3 +11,11 @@ const validateToken = require("../middleware/validateTokenHandler");
 
 // öppen route kräver ingen token för o skapa användare
 router.post("/register", registerUser);
+
+//öppen route ingen token för o logga in
+router.post("/login", loginUser);
+
+//skyddad route krväver JWT-token
+router.get("/current", validateToken, currentUser);
+
+module.exports = router;
