403c005e7c97ba01dabfd5993c95a523b1214396
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-05-29 23:04:42 +0200

    const formadat

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,3 +1,16 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { register } from "../api";
+
+function RegisterPage() {
+    const navigate = useNavigate();
+
+    //sparar användarens input
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    
+}
