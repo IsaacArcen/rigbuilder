84d6ea9cf709bcca07f53c656917d8141a73a79c
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-05-29 23:34:51 +0200

    export RegisterPage

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -24,6 +24,83 @@ function RegisterPage() {
             [name]: value,
         });
     };
-
     
+    //när anvädaren klickar på register
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (!formData.username || !formData.email || !formData.password) {
+            setErrorMessage("Please fill in all fields.");
+            return;
+        }
+
+        try {
+            setErrorMessage("");
+
+            //skickar ny user till backend
+            //POST /api/users/register
+            await register(formData);
+
+            //registrering lyckats
+            navigate("/login");
+        } catch (error) {
+            setErrorMessage(error.message);
+        }
+    };
+
+    return (
+        <section className="auth-page">
+            <div className="auth-card">
+                <p className="auth-card__eyebrow">Account</p>
+                <h1>Create account</h1>
+
+                <form className="auth-form" onSubmit={handleSubmitd}>
+                    <label>
+                        Username
+                        <input
+                            type="text"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleChange}
+                            placeholder="your username"
+                            />
+                    </label>
+
+                    <label>
+                        Email
+                        <input 
+                            type="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            placeholder="you@example.com"
+                            />
+                    </label>
+
+                    <label>
+                        Password
+                        <input
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            placeholder="choose password"
+                            />
+                    </label>
+
+                    {errorMessage && <p className="auth-form__error">{errorMessage}</p>}
+
+                    <button type="submit" className="auth-form__button">
+                        Create account
+                    </button>
+                </form>
+
+                <p className="auth-form__text">
+                    Already have an account? <Link to="/login">Login</Link>
+                </p>
+            </div>
+        </section>
+    );
 }
+
+export default RegisterPage;
