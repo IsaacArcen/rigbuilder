50bec1d7841d499087f86bfecde585d8d173df83
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-05-29 19:12:36 +0200

    const navigate + login const formdata setformadata

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,3 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { login as apiLogin } from "../api";
 import { useAuth } from "../AuthContext";
 
+function LoginPage() {
+    const navigate = useNavigate();
+    const { login } = useAuth();
+
+    //spara det som användaren skriver
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+    });
+}
