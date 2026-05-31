4bdab60c6be8c82467d97b4692690f6da1ed2a6b
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-05-31 12:56:39 +0200

    favcoritesProvider

--- src/context/FavoriteContext.jsx
+++ src/context/FavoriteContext.jsx
@@ -1,3 +1,34 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useAuth } from "./AuthContext";
 import { getFavorites, addFavorite, removeFavorite, } from "../api";
+
+//skapar context objekt
+const FavoritesContext = createContext(null);
+
+//provider komponenten ska omsluta appen i App.js
+export function FavoritesProvider({ children }) {
+    const { authed } = useAuth();
+
+    //sparar produktId som backend skickar tillbaka
+    const [favorites, setFavorites] = useState([]);
+
+    //hämtar favoriter från inloggad användare
+    //om han loggar ut töms listan i frontend
+    useEffect(() => {
+        if (!authed) {
+            setFavorites([]);
+            return;
+        }
+
+        const loadFavorites = async () => {
+            try {
+                const data = await getFavorites();
+                setFavorites(data);
+            } catch (error) {
+                console.error(error.message);
+            }
+        };
+
+        loadFavorites();
+    }, [authed]);
+}
