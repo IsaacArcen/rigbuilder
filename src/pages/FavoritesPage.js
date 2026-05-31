5c21292a46ccde7acc6a6407c200fc2321992ba4
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-05-31 13:23:36 +0200

    const favoritePage

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -5,3 +5,13 @@ import { useFavorites } from "../context/FavoritesContext";
 import { getProducts } from "../api";
 import { useBuild } from "../context/BuildContext";
 
+function FavoritesPage() {
+    const navigate = useNavigate();
+    const { authed } = useAuth();
+    const { favorites, toggleFavorites } = useFavorites();
+    const { selectComponent } = useBuild();
+
+    //här sparas  hela produktobjekten efter hämtats från backend
+    const [products, setProducts] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
+}
