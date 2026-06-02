812e2e64255731684d3efeefa2aeb7c54ae18e3f
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-06-02 11:58:08 +0200

    productModel

---
+++ server/models/productModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+    {
+        productId: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+        category: {
+            type: String,
+            required: true,
+        },
+
+        name: {
+            type: String,
+            required: true,
+        },
+
+        brand: {
+            type: String,
+            required: true,
+        },
+
+        price: {
+            type: Number,
+            required: true,
+        },
+
+        image: {
+            type: String,
+            default: "",
+        },
+
+        specs:  {
+            type: [String],
+            default: [],
+        },
+    },
+
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("Product", productSchema);
