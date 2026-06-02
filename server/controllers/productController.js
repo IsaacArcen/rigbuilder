ab70792a5db12446a979f52d6d108409e0a3d5f4
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-06-02 09:51:13 +0200

    g

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -20,21 +20,6 @@ const getProducts = asyncHandler(async (req, res) => {
     res.status(200).json(products);
 });
 
-// @desc    Get product by ID
-// @route   GET /api/products/:id
-// @access  Public
-const getProductById = asyncHandler(async (req, res) => {
-    const product = products.find((p) => p.id === req.params.id);
-
-    if (!product) {
-        res.status(404);
-        throw new Error("Product not found");
-    }
-
-    res.status(200).json(product);
-});
-
 module.exports = {
     getProducts,
-    getProductById,
 };
