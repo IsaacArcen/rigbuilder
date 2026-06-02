728a00cef2c98522a27fe103984a8176eeec020b
Isaac Arcen <isaac.arcen@edu.newton.se>
2026-06-02 12:59:03 +0200

    fix

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
     {
-        productId: {
+        id: {
             type: String,
             required: true,
             unique: true,
